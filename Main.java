61f22cfe0c5583f4d710a89da0109b28da73d4f0
aellis14 <adriana.ellis@patelhs.org>
2021-02-19 15:41:16 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+class Main {
+  public static void main(String[] args) 
+  {
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Whats your username?");
+    String username = scan.next();
+    System.out.println("Whats your password");
+    String password = scan.next();
+
+    }
+
+    public User(String nameInit, String pwordInit)
+    {
+      username = nameInit;
+      password = pwordInit;
+    }
+    
+    public static int 
+  }
